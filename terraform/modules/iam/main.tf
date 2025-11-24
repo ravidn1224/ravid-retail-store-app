ab7162e9b1b8404725a21cd312be97f125bd9248
Ravid <ravid1242@gmail.com>
2025-11-24 19:48:29 +0200

    testing

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -1,4 +1,8 @@
-variable "project_name" { type = string }
+variable "project_name" {
+  type = string
+}
+
+# EC2 
 
 resource "aws_iam_role" "ec2_role" {
   name               = "${var.project_name}-ec2-role"
@@ -50,3 +54,38 @@ resource "aws_iam_instance_profile" "ec2_profile" {
 }
 
 output "instance_profile_name" { value = aws_iam_instance_profile.ec2_profile.name }
+
+# IAM user with ecr role
+
+resource "aws_iam_user" "ecr_user" {
+  name = "${var.project_name}-ecr-user"
+}
+
+data "aws_iam_policy_document" "user_assume_role_trust" {
+  statement {
+    effect = "Allow"
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "AWS"
+      identifiers = [aws_iam_user.ecr_user.arn]
+    }
+  }
+}
+
+resource "aws_iam_role" "ecr_user_role" {
+  name               = "${var.project_name}-ecr-user-role"
+  assume_role_policy = data.aws_iam_policy_document.user_assume_role_trust.json
+}
+
+resource "aws_iam_role_policy_attachment" "ecr_user_role_attachment" {
+  role       = aws_iam_role.ecr_user_role.name
+  policy_arn = aws_iam_policy.ecr_access.arn
+}
+
+output "ecr_user_name" {
+  value = aws_iam_user.ecr_user.name
+}
+
+output "ecr_user_role_arn" {
+  value = aws_iam_role.ecr_user_role.arn
+}
